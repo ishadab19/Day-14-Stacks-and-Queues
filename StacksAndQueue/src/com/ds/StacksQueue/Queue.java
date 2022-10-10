449d4d6acfff5e5a3addd4ae80c13c6d46882766
shadab <me.shadab19@gmail.com>
2022-10-11 01:32:31 +0530

    [Added- Adding element throuh Enqueue]

--- StacksAndQueue/src/com/ds/StacksQueue/Queue.java
+++ StacksAndQueue/src/com/ds/StacksQueue/Queue.java
@@ -1,10 +1,26 @@
 package com.ds.StacksQueue;
 
 public class Queue {
+	LinkedList list = new LinkedList();
+
+	public void enQueue(Object data) {
+		list.addFirst(data);
+
+	}
+
+	public void display() {
+		list.display();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to Stack and Queue Data Structure");
-
+		Queue queue = new Queue();
+		queue.enQueue(70);
+		queue.enQueue(30);
+		queue.enQueue(56);
+		queue.display();
+		
+				
 	}
 
 }
